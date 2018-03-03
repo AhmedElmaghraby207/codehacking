6a91468193e26f682f972bc4e9b385e33f75879b
Ahmed Elmaghraby <ahmed.elmaghraby207@gmail.com>
2018-03-03 03:08:51 +0200

    Before upgrading to laravel 5.5

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,10 +12,11 @@
     <title>Admin</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
-
+    
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    
     <link href="{{asset('css/dropzone.min.css')}}" rel="stylesheet">
 
 
